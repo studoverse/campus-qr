a76d61f6844830fe9951e81389261217ba34a058
Žiga Vehovec <ziga.veho@gmail.com>
2020-09-08 10:08:27 +0200

    Added are you sure dialogs (#17)
    * Added are you sure dialogs
    
    * Formatting
    
    * Update src/moderatorFrontendMain/kotlin/util/Strings.kt
    
    Co-authored-by: Valentin Slawicek <valentin.slawi@gmail.com>
    
    * Update src/moderatorFrontendMain/kotlin/util/Strings.kt
    
    Co-authored-by: Valentin Slawicek <valentin.slawi@gmail.com>
    
    Co-authored-by: Valentin Slawicek <valentin.slawi@gmail.com>

--- src/moderatorFrontendMain/kotlin/views/users/UserTableRow.kt
+++ src/moderatorFrontendMain/kotlin/views/users/UserTableRow.kt
@@ -5,6 +5,7 @@ import apiBase
 import com.studo.campusqr.common.ClientUser
 import com.studo.campusqr.common.UserData
 import com.studo.campusqr.common.UserType
+import kotlinx.browser.window
 import materialMenu
 import react.*
 import util.Strings
@@ -20,8 +21,8 @@ import kotlin.js.json
 
 interface UserTableRowProps : RProps {
   class Config(
-    val user: ClientUser,
-    val onEditFinished: (response: String?) -> Unit
+      val user: ClientUser,
+      val onEditFinished: (response: String?) -> Unit
   )
 
   var config: Config
@@ -42,42 +43,42 @@ class UserTableRow : RComponent<UserTableRowProps, UserTableRowState>() {
   }
 
   private fun RBuilder.renderEditUserDialog() = mbMaterialDialog(
-    show = true,
-    title = Strings.user_edit.get(),
-    customContent = {
-      renderAddUser(
-        AddUserProps.Config.Edit(props.config.user, onFinished = { response ->
-          setState {
-            if (response == "ok") {
-              showEditUserDialog = false
-              snackbarText = Strings.user_updated_account_details.get()
-            } else {
-              snackbarText = Strings.network_error.get()
-            }
-          }
-          props.config.onEditFinished(response)
-        }),
-        userData = props.userData
-      )
-    },
-    buttons = null,
-    onClose = {
-      setState {
-        showEditUserDialog = false
+      show = true,
+      title = Strings.user_edit.get(),
+      customContent = {
+        renderAddUser(
+            AddUserProps.Config.Edit(props.config.user, onFinished = { response ->
+              setState {
+                if (response == "ok") {
+                  showEditUserDialog = false
+                  snackbarText = Strings.user_updated_account_details.get()
+                } else {
+                  snackbarText = Strings.network_error.get()
+                }
+              }
+              props.config.onEditFinished(response)
+            }),
+            userData = props.userData
+        )
+      },
+      buttons = null,
+      onClose = {
+        setState {
+          showEditUserDialog = false
+        }
       }
-    }
   )
 
   override fun RBuilder.render() {
     mbSnackbar(
-      MbSnackbarProps.Config(
-        show = state.snackbarText.isNotEmpty(),
-        message = state.snackbarText,
-        onClose = {
-          setState {
-            snackbarText = ""
-          }
-        })
+        MbSnackbarProps.Config(
+            show = state.snackbarText.isNotEmpty(),
+            message = state.snackbarText,
+            onClose = {
+              setState {
+                snackbarText = ""
+              }
+            })
     )
     if (state.showEditUserDialog) {
       renderEditUserDialog()
@@ -98,23 +99,24 @@ class UserTableRow : RComponent<UserTableRowProps, UserTableRowState>() {
       }
       mTableCell {
         materialMenu(
-          menuItems = listOf(
-            MenuItem(text = Strings.user_edit.get(), icon = editIcon, onClick = {
-              setState {
-                showEditUserDialog = true
-              }
-            }),
-            MenuItem(text = Strings.user_delete.get(), icon = deleteIcon, onClick = {
-              launch {
-                val response = NetworkManager.post<String>(
-                  "$apiBase/user/delete", params = json(
-                    "userId" to props.config.user.id
-                  )
-                )
-                props.config.onEditFinished(response)
-              }
-            }, enabled = props.config.user.id != props.userData.clientUser!!.id), // Don't delete own user for better UX
-          )
+            menuItems = listOf(
+                MenuItem(text = Strings.user_edit.get(), icon = editIcon, onClick = {
+                  setState {
+                    showEditUserDialog = true
+                  }
+                }),
+                MenuItem(text = Strings.user_delete.get(), icon = deleteIcon, onClick = {
+                  if (window.confirm(Strings.user_delete_are_you_sure.get())) {
+                    launch {
+                      val response = NetworkManager.post<String>(
+                          "$apiBase/user/delete", params = json(
+                          "userId" to props.config.user.id)
+                      )
+                      props.config.onEditFinished(response)
+                    }
+                  }
+                }, enabled = props.config.user.id != props.userData.clientUser!!.id), // Don't delete own user for better UX
+            )
         )
       }
     }
