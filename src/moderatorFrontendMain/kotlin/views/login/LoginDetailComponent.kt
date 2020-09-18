1f0e4da818ed8414edeffb37fd73b95baa32eb02
Filippo Orru <filippoorru@hotmail.com>
2020-09-18 16:30:55 +0200

    Fix formatting

--- src/moderatorFrontendMain/kotlin/views/login/LoginDetailComponent.kt
+++ src/moderatorFrontendMain/kotlin/views/login/LoginDetailComponent.kt
@@ -13,7 +13,7 @@ abstract class LoginDetailComponent<P : react.RProps, S : react.RState>(props: P
     // Use baseUrl of current origin to safely get current hostname and construct href from relative url
     // https://developer.mozilla.org/en-US/docs/Web/API/URL
     val parsedRedirectUrl = URL(/* url = */redirectUrl, /* baseUrl = */window.location.origin)
-      .takeIf { it.hostname == window.location.hostname }
+        .takeIf { it.hostname == window.location.hostname }
 
     window.location.href = parsedRedirectUrl?.href ?: "/admin/" // Fall back if parsedRedirectUrl was invalid
   }
