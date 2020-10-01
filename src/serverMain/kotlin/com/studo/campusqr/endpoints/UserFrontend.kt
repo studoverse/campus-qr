2179776e1045691049d2ebc4ed8e578f9d86df51
Filippo Orru <filippoorru@hotmail.com>
2020-10-01 18:08:01 +0200

    Basic checkout button view

--- src/serverMain/kotlin/com/studo/campusqr/endpoints/UserFrontend.kt
+++ src/serverMain/kotlin/com/studo/campusqr/endpoints/UserFrontend.kt
@@ -28,7 +28,11 @@ suspend fun ApplicationCall.userFrontend() {
 
   respondHtml(HttpStatusCode.OK) {
     lang = language
-    headTemplate("Check In", css = "userFrontend/userFrontend.css", js = "userFrontend/userFrontend.js")
+    headTemplate("Check In", css = "userFrontend/userFrontend.css", js = "userFrontend/userFrontend.js", block = {
+      if (location != null) {
+        meta("location-name", location.name) {}
+      }
+    })
     body {
       noScript {
         +"You need to enable JavaScript to run this app."
@@ -144,82 +148,98 @@ suspend fun ApplicationCall.userFrontend() {
               }
             }
           }
-          div("result-wrapper hidden") {
-            div("result hidden") {
-              id = "result-ok"
-              div("header") {
-                span {
-                  +LocalizedString("Check-in successful!", "Check-in erfolgreich!").get(this@userFrontend)
-                }
-              }
-              div("large") {
-                div("location") {
-                  img {
-                    src = "/static/userFrontend/locationIcon.svg"
-                  }
+          div("results") {
+            div("result-wrapper hidden") {
+              div("result hidden") {
+                id = "result-ok"
+                div("header") {
                   span {
-                    +locationNameWithSeat
+                    +LocalizedString("Check-in successful!", "Check-in erfolgreich!").get(this@userFrontend)
                   }
                 }
-                div("number") {
-                  unsafe {
-                    +"""
-                  <svg>
-                  	<symbol id="s-text">
-                  		<text text-anchor="middle" x="50%" y="90%">${now.date.toString().padStart(2, '0')}</text>
-                  	</symbol>
-
-                  	<g class = "g-ants">
-                  		<use xlink:href="#s-text" class="text-copy"></use>
-                  		<use xlink:href="#s-text" class="text-copy"></use>
-                  		<use xlink:href="#s-text" class="text-copy"></use>
-                  		<use xlink:href="#s-text" class="text-copy"></use>
-                  		<use xlink:href="#s-text" class="text-copy"></use>
-                  	</g>
-                  </svg>
-                """.trimIndent()
+                div("large") {
+                  div("location") {
+                    img {
+                      src = "/static/userFrontend/locationIcon.svg"
+                    }
+                    span {
+                      +locationNameWithSeat
+                    }
+                  }
+                  div("number") {
+                    unsafe {
+                      +"""
+                        <svg>
+                          <symbol id="s-text">
+                            <text text-anchor="middle" x="50%" y="90%">${now.date.toString().padStart(2, '0')}</text>
+                          </symbol>
+      
+                          <g class = "g-ants">
+                            <use xlink:href="#s-text" class="text-copy"></use>
+                            <use xlink:href="#s-text" class="text-copy"></use>
+                            <use xlink:href="#s-text" class="text-copy"></use>
+                            <use xlink:href="#s-text" class="text-copy"></use>
+                            <use xlink:href="#s-text" class="text-copy"></use>
+                          </g>
+                        </svg>
+                      """.trimIndent()
+                    }
+                  }
+                  div("verification") {
+                    span {
+                      +LocalizedString("Verification", "Verifizierung").get(this@userFrontend)
+                    }
                   }
                 }
-                div("verification") {
-                  span {
-                    +LocalizedString("Verification", "Verifizierung").get(this@userFrontend)
+                div("details") {
+                  div("row") {
+                    span("datetime") {
+                      +LocalizedString(" at ", " um ").get(this@userFrontend)
+                    }
                   }
-                }
-              }
-              div("details") {
-                div("row") {
-                  span("datetime") {
-                    +LocalizedString(" at ", " um ").get(this@userFrontend)
-                  }
-                }
-                div("row") {
-                  span("identification") {
-                    id = "result-ok-id"
+                  div("row") {
+                    span("identification") {
+                      id = "result-ok-id"
+                    }
                   }
                 }
               }
             }
+            span("result fail hidden") {
+              id = "result-forbidden-access-restricted"
+              +LocalizedString(
+                "Error! You are not allowed to check-in.",
+                "Fehler! Sie haben keine Berechtigung um einzuchecken."
+              ).get(this@userFrontend)
+            }
+            span("result fail hidden") {
+              id = "result-forbidden-email"
+              +LocalizedString(
+                "Error! Please use your university e-mail address to check-in.",
+                "Fehler! Bitte benutzen Sie ihre Hochschul E-Mail Adresse um einzuchecken."
+              ).get(this@userFrontend)
+            }
+            span("result fail hidden") {
+              id = "result-net-err"
+              +LocalizedString(
+                "Error! Please try again.",
+                "Fehler! Bitte versuchen Sie es erneut."
+              ).get(this@userFrontend)
+            }
           }
-          span("result fail hidden") {
-            id = "result-forbidden-access-restricted"
-            +LocalizedString(
-              "Error! You are not allowed to check-in.",
-              "Fehler! Sie haben keine Berechtigung um einzuchecken."
-            ).get(this@userFrontend)
-          }
-          span("result fail hidden") {
-            id = "result-forbidden-email"
-            +LocalizedString(
-              "Error! Please use your university e-mail address to check-in.",
-              "Fehler! Bitte benutzen Sie ihre Hochschul E-Mail Adresse um einzuchecken."
-            ).get(this@userFrontend)
-          }
-          span("result fail hidden") {
-            id = "result-net-err"
-            +LocalizedString(
-              "Error! Please try again.",
-              "Fehler! Bitte versuchen Sie es erneut."
-            ).get(this@userFrontend)
+          div("hidden") {
+            id = "checkins-wrapper"
+            h3 {
+              +LocalizedString("Active check-ins", "Aktive check-ins").get(this@userFrontend)
+            }
+            div("current-check-in hidden") {
+              span {
+                +""
+              }
+              button {
+                +LocalizedString("Check out", "Auschecken").get(this@userFrontend)
+              }
+            }
           }
         }
       }
