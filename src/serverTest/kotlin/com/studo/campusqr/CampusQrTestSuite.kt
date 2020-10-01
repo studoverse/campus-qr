8341e9fb7316639baf5f02dec81311ac7b30f766
Zoltan Sasvari <sasvarizoli@me.com>
2020-10-01 18:09:02 +0200

    test for automatic check out

--- src/serverTest/kotlin/com/studo/campusqr/CampusQrTestSuite.kt
+++ src/serverTest/kotlin/com/studo/campusqr/CampusQrTestSuite.kt
@@ -9,7 +9,8 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 
 @Suite.SuiteClasses(
-    AutomaticDeletionTest::class
+    AutomaticDeletionTest::class,
+    AutomaticCheckOutTest::class,
 )
 
 object CampusQrTestSuite {
