a2895453300d988a7151eea331becc354ccff072
Filippo Orrù <33554869+ffactory-ofcl@users.noreply.github.com>
2021-08-18 17:26:46 +0200

    Workaround for kotlinJs bug: hardcode kotlinJs webpack version (#101)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,10 @@ plugins {
   kotlin("js") version "1.5.10" apply false
   kotlin("jvm") version "1.5.10" apply false
 }
+
+// Workaround. Hardcode the webpack version: 4rc0 works fine while 5 doesn't.
+// Bug report & details https://youtrack.jetbrains.com/issue/KT-48273
+// The fix was published in the experimental Kotlin version 1.5.30-382. TODO: Remove this workaround when 1.5.30 is stable
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
+  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackDevServer.version = "4.0.0-rc.0"
+}
