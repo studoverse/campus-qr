e6d8bfb92a52dfc1a244f04c0b34aebce58d76cf
Filippo Orru <filippoorru@hotmail.com>
2021-08-18 16:20:41 +0200

    Fix moderatorFrontend webpack bug Hardcode webpack version to 4rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,9 @@ plugins {
   kotlin("js") version "1.5.10" apply false
   kotlin("jvm") version "1.5.10" apply false
 }
+
+// Workaround. Hardcode the webpack version: 4rc0 works fine while 5 doesn't. Remove when fixed.
+// Bug report & details https://youtrack.jetbrains.com/issue/KT-48273
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
+  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackDevServer.version = "4.0.0-rc.0"
+}
