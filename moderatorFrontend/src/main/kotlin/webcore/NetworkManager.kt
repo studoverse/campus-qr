0fd541144bdfb96e27297d98505d046ccf8cf0fe
Moritz Hofmeister <moritz.hofmeister@studo.com>
2021-09-03 16:49:31 +0200

    Remove obsolete list and map hacks for serialization

--- moderatorFrontend/src/main/kotlin/webcore/NetworkManager.kt
+++ moderatorFrontend/src/main/kotlin/webcore/NetworkManager.kt
@@ -10,7 +10,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
 import kotlinx.browser.window
-import kotlinx.serialization.json.*
 
 /**
  * NetworkManager uses Ktor client with kotlinx.serialization to create network requests.
@@ -44,9 +43,6 @@ object NetworkManager {
     null
   }
 
-  /**
-   * [body] Map<String, Any?>, List<Any?> or @Serializable
-   */
   suspend inline fun <reified T : Any> post(
     url: String,
     body: Any? = null,
@@ -56,14 +52,7 @@ object NetworkManager {
     val response: HttpResponse = client.post(url) {
       body?.let {
         contentType(ContentType.Application.Json)
-        this.body = when (body) {
-          // Kotlinx's serialization doesn't support serialization of collections with mixed types,
-          // see https://youtrack.jetbrains.com/issue/KTOR-3063.
-          // So handle maps and lists with our toJsonElement()
-          is Map<*, *> -> body.toJsonElement()
-          is List<*> -> body.toJsonElement()
-          else -> body // Let the Ktor client handle @Serializable classes.
-        }
+        this.body = body // Let the Ktor client handle @Serializable classes.
       }
       urlParams.forEach { parameter(it.key, it.value) }
       headers.forEach { header(it.key, it.value) }
@@ -106,40 +95,5 @@ object NetworkManager {
         window.location.reload()
       }
     }
-  }
-
-  fun List<*>.toJsonElement(): JsonElement {
-    val list: MutableList<JsonElement> = mutableListOf()
-    this.forEach { value ->
-      when (value) {
-        null -> list.add(JsonNull)
-        is Map<*, *> -> list.add(value.toJsonElement())
-        is List<*> -> list.add(value.toJsonElement())
-        is Boolean -> list.add(JsonPrimitive(value))
-        is Number -> list.add(JsonPrimitive(value))
-        is String -> list.add(JsonPrimitive(value))
-        is Enum<*> -> list.add(JsonPrimitive(value.toString()))
-        else -> throw IllegalStateException("Can't serialize unknown collection type: $value")
-      }
-    }
-    return JsonArray(list)
-  }
-
-  fun Map<*, *>.toJsonElement(): JsonElement {
-    val map: MutableMap<String, JsonElement> = mutableMapOf()
-    this.forEach { (key, value) ->
-      key as String
-      when (value) {
-        null -> map[key] = JsonNull
-        is Map<*, *> -> map[key] = value.toJsonElement()
-        is List<*> -> map[key] = value.toJsonElement()
-        is Boolean -> map[key] = JsonPrimitive(value)
-        is Number -> map[key] = JsonPrimitive(value)
-        is String -> map[key] = JsonPrimitive(value)
-        is Enum<*> -> map[key] = JsonPrimitive(value.toString())
-        else -> throw IllegalStateException("Can't serialize unknown type: $value")
-      }
-    }
-    return JsonObject(map)
   }
 }
