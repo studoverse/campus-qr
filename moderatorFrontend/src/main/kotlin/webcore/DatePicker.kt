6a013865237b322a4432b2f5a7478e76eef52a7d
Moritz Hofmeister <moritz.hofmeister@studo.com>
2021-09-06 17:05:40 +0200

    Native Date- & TimePicker components (WIP)

---
+++ moderatorFrontend/src/main/kotlin/webcore/DatePicker.kt
@@ -0,0 +1,240 @@
+package webcore
+
+import com.studo.campusqr.common.utils.LocalizedString
+import kotlinx.browser.document
+import kotlinx.html.InputType
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
+import react.*
+import react.dom.div
+import util.get
+import webcore.extensions.emptyToNull
+import webcore.extensions.inputValue
+import webcore.extensions.setFullYear
+import webcore.extensions.toInputTypeDateValueString
+import webcore.materialUI.textField
+import webcore.materialUI.withStyles
+import kotlin.js.Date
+
+interface DatePickerProps : RProps {
+  var classes: DatePickerClasses
+  var date: Date
+  var onChange: (date: Date, isValid: Boolean) -> Unit
+  var disabled: Boolean
+  var error: Boolean
+  var min: Date?
+  var max: Date?
+}
+
+interface DatePickerState : RState {
+  var dateTimeInputValue: String
+  var oldBrowsersInputValues: DatePicker.DateInputValues
+  var fieldError: Boolean
+}
+
+class DatePicker(props: DatePickerProps) : RComponent<DatePickerProps, DatePickerState>(props) {
+
+  class DateInputValues(var year: String, var month: String, var day: String)
+
+  override fun DatePickerState.init(props: DatePickerProps) {
+    val year = props.date.getFullYear()
+    val month = props.date.getMonth() + 1
+    val day = props.date.getDate()
+    dateTimeInputValue = props.date.toInputTypeDateValueString()
+    oldBrowsersInputValues = DateInputValues(year.toString(), month.toString(), day.toString())
+    fieldError = false
+  }
+
+  override fun componentDidUpdate(prevProps: DatePickerProps, prevState: DatePickerState, snapshot: Any) {
+    if (prevProps.date != props.date) {
+      setState { init(props) }
+    }
+  }
+
+  private fun daysInMonth(month: Int, year: Int): Int {
+    return Date(year, month, 0).getDate()
+  }
+
+  private fun detectInputDateSupport(): Boolean {
+    val input = document.createElement("input") as HTMLInputElement
+    input.setAttribute("type", "date")
+    val invalidDateValue = "not-a-date"
+    input.setAttribute("value", invalidDateValue)
+    return input.value != invalidDateValue
+  }
+
+  private fun tryParsingInputFields(dayInputValue: String, monthInputValue: String, yearInputValue: String) {
+    val now = Date()
+    var day: Int = now.getDay()
+    var month: Int = now.getMonth()
+    var year: Int = now.getFullYear()
+    var isValid: Boolean
+    try {
+      day = dayInputValue.toInt()
+      month = monthInputValue.toInt() - 1 // 0 - based month [0-11]
+      year = yearInputValue.toInt()
+
+      isValid = !(day > 31 || day < 1 || month < 0 || month > 11 || year !in 1000..9999 || daysInMonth(month, year) < day)
+    } catch (e: Throwable) {
+      isValid = false
+    }
+
+    setState {
+      fieldError = !isValid
+    }
+    val date = Date().setFullYear(year, month, day)
+    props.onChange(date, isValid)
+  }
+
+  private fun RBuilder.renderWithInputTypeDateSupport() {
+    textField {
+      attrs.type = InputType.date.toString()
+      attrs.value = state.dateTimeInputValue
+      attrs.inputProps = kotlinext.js.js {
+        props.min?.let { minProp ->
+          min = minProp.toInputTypeDateValueString()
+        }
+        props.max?.let { maxProp ->
+          max = maxProp.toInputTypeDateValueString()
+        }
+      }
+      attrs.disabled = props.disabled
+      attrs.error = props.error || state.fieldError
+      attrs.onChange = { event: Event ->
+        event.inputValue.emptyToNull()?.let { value ->
+          setState {
+            dateTimeInputValue = value
+          }
+          val inputElement = event.target as HTMLInputElement
+          val inputDateTimestamp = inputElement.valueAsNumber
+          val date = Date(inputDateTimestamp)
+
+          val isValid: Boolean = !inputDateTimestamp.isNaN() && date.getFullYear() > 1000 && date.getFullYear() < 9999
+          props.onChange(date, isValid)
+          setState { fieldError = !isValid }
+        }
+      }
+    }
+  }
+
+  private fun RBuilder.renderWithoutInputTypeDateSupport() {
+    val dayString = LocalizedString(
+      "Day",
+      "Tag"
+    ).get()
+    val monthString = LocalizedString(
+      "Month",
+      "Monat"
+    ).get()
+    val yearString = LocalizedString(
+      "Year",
+      "Jahr"
+    ).get()
+    div(props.classes.flex) {
+      textField {
+        attrs.disabled = props.disabled
+        attrs.error = props.error || state.fieldError
+        attrs.style = kotlinext.js.js { flex = 1 }
+        attrs.classes = kotlinext.js.js {
+          root = props.classes.textFieldLabel
+        }
+        attrs.type = InputType.number.toString()
+        attrs.placeholder = dayString
+        attrs.label = dayString
+        attrs.inputProps = kotlinext.js.js {
+          min = 1
+          max = 31
+        }
+        attrs.value = state.oldBrowsersInputValues.day
+        attrs.onChange = { event: Event ->
+          val value = event.inputValue
+          setState { oldBrowsersInputValues.day = value }
+          tryParsingInputFields(value, state.oldBrowsersInputValues.month, state.oldBrowsersInputValues.year)
+        }
+      }
+      textField {
+        attrs.disabled = props.disabled
+        attrs.error = props.error || state.fieldError
+        attrs.style = kotlinext.js.js { flex = 2 }
+        attrs.classes = kotlinext.js.js {
+          root = props.classes.textFieldLabel
+        }
+        attrs.type = InputType.number.toString()
+        attrs.placeholder = monthString
+        attrs.label = monthString
+        attrs.inputProps = kotlinext.js.js {
+          min = 1
+          max = 12
+        }
+        attrs.value = state.oldBrowsersInputValues.month
+        attrs.onChange = { event: Event ->
+          val value = event.inputValue
+          setState { oldBrowsersInputValues.month = value }
+          tryParsingInputFields(state.oldBrowsersInputValues.day, value, state.oldBrowsersInputValues.year)
+        }
+      }
+      textField {
+        attrs.disabled = props.disabled
+        attrs.error = props.error || state.fieldError
+        attrs.style = kotlinext.js.js { flex = 2 }
+        attrs.classes = kotlinext.js.js {
+          root = props.classes.textFieldLabel
+        }
+        attrs.type = InputType.number.toString()
+        attrs.placeholder = yearString
+        attrs.label = yearString
+        attrs.value = state.oldBrowsersInputValues.year
+        attrs.onChange = { event: Event ->
+          val value = event.inputValue
+          setState { oldBrowsersInputValues.year = value }
+          tryParsingInputFields(state.oldBrowsersInputValues.day, state.oldBrowsersInputValues.month, value)
+        }
+      }
+    }
+  }
+
+  override fun RBuilder.render() {
+    div {
+      if (detectInputDateSupport()) {
+        renderWithInputTypeDateSupport()
+      } else {
+        renderWithoutInputTypeDateSupport()
+      }
+    }
+  }
+}
+
+interface DatePickerClasses {
+  var flex: String
+  var textFieldLabel: String
+}
+
+private val styles = { theme: dynamic ->
+  kotlinext.js.js {
+    flex = kotlinext.js.js {
+      display = "flex"
+      width = "100%"
+    }
+    textFieldLabel = kotlinext.js.js {
+      paddingTop = theme.spacing(2)
+    }
+  }
+}
+
+private val styled = withStyles<DatePickerProps, DatePicker>(styles)
+
+fun RBuilder.datePicker(
+  date: Date,
+  onChange: (date: Date, isValid: Boolean) -> Unit,
+  disabled: Boolean = false,
+  error: Boolean = false,
+  min: Date? = null,
+  max: Date? = null,
+) = styled {
+  attrs.date = date
+  attrs.onChange = onChange
+  attrs.disabled = disabled
+  attrs.error = error
+  attrs.min = min
+  attrs.max = max
+}
---
+++ moderatorFrontend/src/main/kotlin/webcore/TimePicker.kt
@@ -0,0 +1,212 @@
+package webcore
+
+import com.studo.campusqr.common.utils.LocalizedString
+import kotlinx.browser.document
+import kotlinx.html.InputType
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
+import react.*
+import react.dom.datalist
+import react.dom.div
+import react.dom.option
+import util.get
+import webcore.extensions.*
+import webcore.materialUI.textField
+import webcore.materialUI.withStyles
+import kotlin.js.Date
+
+interface TimePickerProps : RProps {
+  var classes: TimePickerClasses
+  var time: Date
+  var onChange: (date: Date) -> Unit
+  var disabled: Boolean
+  var error: Boolean
+  var min: Date?
+  var max: Date?
+  var stepMinutes: Int
+}
+
+interface TimePickerState : RState {
+  var oldBrowsersInputValues: TimePicker.TimeInputValues
+}
+
+class TimePicker(props: TimePickerProps) : RComponent<TimePickerProps, TimePickerState>(props) {
+
+  private val stepListId = "timestep-list${hashCode()}"
+
+  class TimeInputValues(var hour: String, var minute: String)
+
+  override fun TimePickerState.init(props: TimePickerProps) {
+    val timeValue = props.time.toInputTypeTimeValueString()
+    oldBrowsersInputValues = TimeInputValues(timeValue.split(":")[0], timeValue.split(":")[1])
+  }
+
+  override fun componentDidUpdate(prevProps: TimePickerProps, prevState: TimePickerState, snapshot: Any) {
+    if (prevProps.time != props.time) {
+      setState { init(props) }
+    }
+  }
+
+  private fun detectInputTimeSupport(): Boolean {
+    val input = document.createElement("input") as HTMLInputElement
+    input.setAttribute("type", "time")
+    val invalidDateValue = "not-a-time"
+    input.setAttribute("value", invalidDateValue)
+    return input.value != invalidDateValue
+  }
+
+  private fun tryParsingInputFields(hourInputValue: String, minuteInputValue: String) {
+    try {
+      val hour = hourInputValue.toInt()
+      val minute = minuteInputValue.toInt()
+      if (hour > 23 || hour < 0 || minute < 0 || minute > 59) {
+        // Don't allow invalid input
+        return
+      }
+      val date = Date().with(hour = hour, minute = minute, second = 0, millisecond = 0)
+      props.onChange(date)
+    } catch (e: Exception) {
+    }
+  }
+
+  private fun RBuilder.renderWithInputTypeTimeSupport() {
+    div(props.classes.container) {
+      textField {
+        attrs.value = props.time.toInputTypeTimeValueString()
+        attrs.inputProps = kotlinext.js.js {
+          props.min?.let { minProp ->
+            min = minProp.toInputTypeTimeValueString()
+          }
+          props.max?.let { maxProp ->
+            max = maxProp.toInputTypeTimeValueString()
+          }
+          list = stepListId
+        }
+        attrs.disabled = props.disabled
+        attrs.error = props.error
+        attrs.type = InputType.time.toString()
+        attrs.onChange = { event: Event ->
+          event.inputValue.emptyToNull()?.let { value ->
+            val hourToMinute = value.split(":").map { it.removePrefix("0").toInt() }
+            val hour = hourToMinute[0]
+            val minute = hourToMinute[1]
+            props.onChange(props.time.with(hour = hour, minute = minute))
+          }
+        }
+      }
+      if (props.stepMinutes != 1) { // Create time options list if step is not the default value of 1 minute
+        datalist {
+          attrs["id"] = stepListId
+          // Control variable to fill options list
+          var timestampIterControl = Date().startOfTheDay().getTime()
+          val endOfDay = Date(timestampIterControl).endOfTheDay().getTime()
+          while (timestampIterControl <= endOfDay) {
+            option {
+              val currentDate = Date(timestampIterControl)
+              attrs.value = currentDate.getHours().toString().padStart(2, '0') + ":" +
+                  currentDate.getMinutes().toString().padStart(2, '0') // HH:mm
+            }
+            timestampIterControl += (props.stepMinutes * 60 * 1000) // Minutes to milliseconds
+          }
+        }
+      }
+    }
+  }
+
+  private fun RBuilder.renderWithoutInputTypeTimeSupport() {
+    val hourString = LocalizedString(
+      "Hour",
+      "Stunde"
+    ).get()
+    val minuteString = LocalizedString(
+      "Minute",
+      "Minute"
+    ).get()
+    div(props.classes.container) {
+      textField {
+        attrs.disabled = props.disabled
+        attrs.error = props.error
+        attrs.style = kotlinext.js.js { flex = 1 }
+        attrs.classes = kotlinext.js.js {
+          root = props.classes.textFieldLabel
+        }
+        attrs.type = InputType.number.toString()
+        attrs.placeholder = hourString
+        attrs.label = hourString
+        attrs.inputProps = kotlinext.js.js {
+          min = 0
+          max = 23
+        }
+        attrs.value = state.oldBrowsersInputValues.hour
+        attrs.onChange = { event: Event ->
+          val value = event.inputValue
+          tryParsingInputFields(value, state.oldBrowsersInputValues.minute)
+        }
+      }
+      textField {
+        attrs.disabled = props.disabled
+        attrs.error = props.error
+        attrs.style = kotlinext.js.js { flex = 1 }
+        attrs.classes = kotlinext.js.js {
+          root = props.classes.textFieldLabel
+        }
+        attrs.type = InputType.number.toString()
+        attrs.placeholder = minuteString
+        attrs.label = minuteString
+        attrs.inputProps = kotlinext.js.js {
+          min = 0
+          max = 59
+        }
+        attrs.value = state.oldBrowsersInputValues.minute
+        attrs.onChange = { event: Event ->
+          val value = event.inputValue
+          tryParsingInputFields(state.oldBrowsersInputValues.hour, value)
+        }
+      }
+    }
+  }
+
+  override fun RBuilder.render() {
+    if (detectInputTimeSupport()) {
+      renderWithInputTypeTimeSupport()
+    } else {
+      renderWithoutInputTypeTimeSupport()
+    }
+  }
+}
+
+interface TimePickerClasses {
+  var container: String
+  var textFieldLabel: String
+}
+
+private val TimePickerStyle = { theme: dynamic ->
+  kotlinext.js.js {
+    container = kotlinext.js.js {
+      width = "100%"
+    }
+    textFieldLabel = kotlinext.js.js {
+      paddingTop = theme.spacing(2)
+    }
+  }
+}
+
+private val styled = withStyles<TimePickerProps, TimePicker>(TimePickerStyle)
+
+fun RBuilder.timePicker(
+  time: Date,
+  onChange: (date: Date) -> Unit,
+  disabled: Boolean = false,
+  error: Boolean = false,
+  min: Date? = null,
+  max: Date? = null,
+  stepMinutes: Int = 1
+) = styled {
+  attrs.time = time
+  attrs.onChange = onChange
+  attrs.disabled = disabled
+  attrs.error = error
+  attrs.min = min
+  attrs.max = max
+  attrs.stepMinutes = stepMinutes
+}
