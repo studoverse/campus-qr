a97866524109e66b7723e9b1c99adde5add26af5
Moritz Hofmeister <moritz.hofmeister@studo.com>
2022-03-02 16:31:43 +0100

    Make Index.kt compatible with new wrapper versions

--- moderatorFrontend/src/main/kotlin/index/Index.kt
+++ moderatorFrontend/src/main/kotlin/index/Index.kt
@@ -1,17 +1,25 @@
 package index
 
-import app.app
 import kotlinext.js.require
 import kotlinext.js.requireAll
 import kotlinx.browser.document
+import react.Fragment
+import react.create
 import react.dom.render
+import views.common.renderLinearProgress
+
+fun App() {}
 
 fun main() {
   requireAll(require.context("../../../../../moderatorFrontend/src/main/kotlin", true, js("/\\.css$/")))
   requireAll(require.context("normalize.css", true, js("/\\.css$/")))
 
-  render(document.getElementById("root")!!) {
-    app()
-    //renderLinearProgressNew()
+  val container = document.getElementById("root")!!
+
+  val application = Fragment.create {
+    renderLinearProgress(true)
+    //app()
   }
+
+  render(application, container)
 }
