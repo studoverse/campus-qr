f42253d5eb9b7e5ddc054c5cc5c91aa8905f2787
Zoltán Sasvári <sasvarizoli@me.com>
2022-06-13 18:52:32 +0200

    migrated to kotlin 1.7.0 (#126)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
   }
 }
 
 plugins {
-  val kotlinVersion = "1.6.21"
+  val kotlinVersion = "1.7.0"
   kotlin("multiplatform") version kotlinVersion apply false
   kotlin("jvm") version kotlinVersion apply false
   kotlin("js") version kotlinVersion apply false
