985334569c7d54164ca10a939f16281e6de185bb
Valentin Slawicek <valentin.slawi@gmail.com>
2022-12-01 11:22:33 +0100

    Auto-download JDK for developers

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
   }
 }
 
 plugins {
-  val kotlinVersion = "1.7.20"
+  val kotlinVersion = "1.7.21"
   kotlin("multiplatform") version kotlinVersion apply false
   kotlin("jvm") version kotlinVersion apply false
   kotlin("js") version kotlinVersion apply false
