ab0bb1d942ea2bd627fb65e3de07b491aaad9913
Moritz Hofmeister <62036655+hofi99@users.noreply.github.com>
2023-06-16 10:59:06 +0200

    Backport copyToClipboard (#147)

--- moderatorFrontend/src/main/kotlin/webcore/extensions/String.kt
+++ moderatorFrontend/src/main/kotlin/webcore/extensions/String.kt
@@ -1,5 +1,9 @@
 package webcore.extensions
 
+import js.core.asList
+import mui.material.MuiPaper
+import web.dom.document
+import web.html.HTMLTextAreaElement
 import web.navigator.navigator
 
 val emailRegex = Regex("[^,;:<> ]+@[^,;:<> ]+\\.[^,;:<> ]+")
@@ -30,16 +34,58 @@ fun String.format(vararg parameters: String): String {
   return resultBuilder.toString()
 }
 
-fun String.copyToClipboard(onFulfilled: (() -> Unit)? = null, onRejected: ((throwable: Throwable) -> Unit)? = null) {
-  navigator.clipboard.writeText(this).then(
-    onFulfilled = {
-      onFulfilled?.invoke()
-    },
-    onRejected = { throwable ->
-      onRejected?.invoke(throwable)
-    },
-  )
+fun String.copyToClipboard(onSuccess: (() -> Unit)? = null, onFail: ((throwable: Throwable) -> Unit)? = null) {
+  // Check for navigator.clipboard.writeText support
+  @Suppress("UnsafeCastFromDynamic")
+  if (navigator.clipboard.asDynamic().writeText) {
+    navigator.clipboard.writeText(this).then(
+      onFulfilled = {
+        onSuccess?.invoke()
+      },
+      onRejected = { throwable ->
+        onFail?.invoke(throwable)
+      },
+    )
+  } else {
+    // Some old browsers (including Safari up to iOS 13) don't support navigator.clipboard.writeText
+    // https://caniuse.com/mdn-api_clipboard_writetext
+    // Use execCommand in this case but only for older browsers since it is deprecated.
+    // Below code from https://stackoverflow.com/a/30810322/16739173
+    val textArea = document.createElement("textarea") as HTMLTextAreaElement
+    textArea.style.top = "0"
+    textArea.style.left = "0"
+    textArea.style.position = "fixed"
+    textArea.style.width = "2em"
+    textArea.style.height = "2em"
+    textArea.style.opacity = "0"
+    textArea.style.zIndex = "-1"
+    textArea.value = this
+    // In order to copy, we need to insert the textarea, focus it, and execCommand("copy").
+    // If we are showing a modal component (Dialog, Drawer, Menu, Popover),
+    // we need to insert the textarea within the Focus Trap of the modal, or else focusing it will not work.
+    val focusableElement = document
+      .getElementsByClassName("MuiModal-root")
+      .asList()
+      .singleOrNull { it.ariaHidden != true.toString() } // Try to find active modal, which doesn't have ariaHidden set
+      ?.getElementsByClassName(MuiPaper.root.toString())?.get(0) // Paper is present in all components that use a Modal
+      ?: document.body // Default case when we are not showing a modal component
+    focusableElement.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
+    try {
+      if (document.asDynamic().execCommand("copy")) {
+        onSuccess?.invoke()
+      } else {
+        onFail?.invoke(Throwable(message = "Copy to clipboard failed"))
+      }
+    } catch (exception: Exception) {
+      onFail?.invoke(exception)
+    }
+    focusableElement.removeChild(textArea)
+  }
 }
 
+external fun encodeURI(uri: String): String
+external fun decodeURI(encodedUri: String): String
 external fun decodeURIComponent(encodedUriComponent: String): String
 external fun encodeURIComponent(uriComponent: String): String
