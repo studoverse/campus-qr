65411b7e161e354bd5132e8b64bc8bee53f030cd
Moritz Hofmeister <moritz.hofmeister@studo.com>
2024-10-22 16:43:20 +0200

    Shared file and live templates to simplify DX

---
+++ .idea/fileTemplates/ReactFcTemplate.kt
@@ -0,0 +1,17 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+#end
+
+import react.Props
+import webcore.FcWithCoroutineScope
+
+class ${NAME}Config(
+  
+)
+
+external interface ${NAME}Props : Props {
+  var config: ${NAME}Config
+}
+
+val ${NAME} = FcWithCoroutineScope<${NAME}Props> { props, launch ->
+  val controller = ${NAME}Controller.use${NAME}Controller(launch = launch)
+}
