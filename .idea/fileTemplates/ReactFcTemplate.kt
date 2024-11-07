3be1966976c0043cc6343778269c456f5f621306
Moritz Hofmeister <moritz.hofmeister@studo.com>
2024-11-07 10:33:14 +0100

    Final template adjustments

--- .idea/fileTemplates/ReactFcTemplate.kt
+++ .idea/fileTemplates/ReactFcTemplate.kt
@@ -3,11 +3,13 @@
 
 import react.Props
 import webcore.FcWithCoroutineScope
+import js.lazy.Lazy
 
 external interface ${NAME}Props : Props {
   var config: ${NAME}Config
 }
 
+@Lazy
 val ${NAME} = FcWithCoroutineScope<${NAME}Props> { props, launch ->
   val controller = ${NAME}Controller.use${NAME}Controller(launch = launch)
 }
