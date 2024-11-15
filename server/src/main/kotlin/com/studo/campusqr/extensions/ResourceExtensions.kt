78c8a17f1953be44867606d4ba993d1dcbdd9efc
Moritz Hofmeister <moritz.hofmeister@studo.com>
2024-11-15 16:58:24 +0100

    Add comment for getDirectoryContentPath

--- server/src/main/kotlin/com/studo/campusqr/extensions/ResourceExtensions.kt
+++ server/src/main/kotlin/com/studo/campusqr/extensions/ResourceExtensions.kt
@@ -13,7 +13,8 @@ fun getDirectoryContentPath(path: String): List<String>? {
     File(resource.toURI()).listFiles()?.map { it.name }
   } else if (resource.protocol == "jar") {
     // Production: List files inside the JAR
-    val jarFilePath = resource.path.substringBefore("!")
+    val jarFilePath = resource.path
+      .substringBefore("!")
       .substringAfter("file:")
     val jarFile = java.util.jar.JarFile(URLDecoder.decode(jarFilePath, "UTF-8"))
     val entries = jarFile.entries()
@@ -21,6 +22,7 @@ fun getDirectoryContentPath(path: String): List<String>? {
     while (entries.hasMoreElements()) {
       val entry = entries.nextElement()
       if (entry.name.startsWith(path.removePrefix("/")) && !entry.isDirectory) {
+        // The JarFile provides a complete list of all entries in the archive, so we make sure to only add files that are in [path].
         files.add("/" + entry.name) // Add leading '/' for consistency
       }
     }
